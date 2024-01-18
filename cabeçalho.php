4fe8b506837dfffb0cf73bf31730b42af9030b38
LuizFernandoWolffdeSouza <Wolfluid@gmail.com>
2024-01-18 17:06:22 -0300

    projetophp1

---
+++ cabeçalho.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ <style>
+        body{
+            background:radial-gradient(rgb(4, 4, 70), rgb(0, 250, 250));
+    }
+        img{
+            
+            justify-content:center;
+            margin:0;
+            width:400px;
+            height:400px;
+            border-radius:40px;
+            text-align:center;
+    }
+    
+        div{
+            color:red;
+
+    }
+    h1{
+        background:radial-gradient(rgb(4, 4, 70), rgb(0, 250, 250));
+        color:rgb(4, 4, 137);
+        
+    }
+    </style>
+
+</head>
+<body>
+    <img src="logo.jpg" alt="">
+
+       
+    <h1>sistema de controle
+    </h1>
+    
+</body>
+</html>
